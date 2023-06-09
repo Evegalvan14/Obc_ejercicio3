a2e49f1486357792827210892c08778eb4808f16
Evelin Galvan <evelingalvan.eg@hotmail.com>
2023-06-09 11:17:47 -0300

    ejercicio 3

---
+++ factorial-break.js
@@ -0,0 +1,12 @@
+let dato = 10
+let resultado = dato
+let i = 1                                    
+
+while (true) {
+    if (i > dato) {
+        break;
+    }
+    resultado *= i
+    i++
+    console.log(resultado)
+}
